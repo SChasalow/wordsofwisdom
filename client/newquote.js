735ca836b8464daa360859822861665b5c9365e0
elyisgreat <elyisgreat2@gmail.com>
2015-07-17 10:19:04 -0400

    added tags and fixed show more button
    -changed header
    -removed “show less” button and made “show more” button automatic
    -added tags
    -changed the add quote button

--- client/newquote.js
+++ client/newquote.js
@@ -1,29 +1,37 @@
-// This is the code for the newquote template.  
+// This is the code for the newquote template.
 // It adds a new quote to the database when the user presses the save button.
 // It has three local variables which store the author, quote, and likes values
 // when the user modifies one of these fields in the browser, the local value is stored here
 // when they press on the save button, the new quote is created and inserted into the Quotes collection
- 
-  
+
+
   Template.newquote.events({
-	  
+
 	  'click button#saveQuote': function(event){
 		  // read the author and quote text from the browser window
-		  var newQuoteText = $("#newquotetext").val();	
-		 
-		  
-		  // and erase the fields so the user can add another quote later	
-		  $("#newquotetext").val(" ");	
-		  Meteor.user().emails[0].address				
-		  
+		  var newQuoteText = $("#newquotetext").val();
+      var newtaglist = ($("#newtags").val()).split(",");
+      var newTags = [];
+      $.each(newtaglist, function(i, el){
+    if($.inArray(el, newTags) === -1) newTags.push(el);
+});
+
+
+
+		  // and erase the fields so the user can add another quote later
+		  $("#newquotetext").val("");
+      $("#newtags").val("");
+		  Meteor.user().emails[0].address
+
 		  // create a new quote object and upload it to the server!
-		  var quote = 
-		  	{quote:newQuoteText,  
-		  		likes:0, 
+		  var quote =
+		  	{quote:newQuoteText,
+          tags:newTags,
+		  		likes:0,
 		  		createdAt: new Date(),
 		  		createdBy: Meteor.user().emails[0].address,
 		  		user:Meteor.userId()};  // create the JSON object representing the quote
-		  Quotes.insert(quote); 
+		  Quotes.insert(quote);
 	  }
-  	
+
   })
